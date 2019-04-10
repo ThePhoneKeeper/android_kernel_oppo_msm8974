e558c05a418f1bf92540a44206d7405e1945029b
Rishab Nair <rishab9706@gmail.com>
2019-04-10 11:28:00 +0000

    Revert "kernel: Boost all CPUs to the max when userspace launches an app"
    This reverts commit 026ab86722ddd87c29ac32ff68393a2983a76a8a.

--- kernel/fork.c
+++ kernel/fork.c
@@ -69,7 +69,6 @@
 #include <linux/oom.h>
 #include <linux/khugepaged.h>
 #include <linux/signalfd.h>
-#include <linux/cpu_input_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1629,10 +1628,6 @@ long do_fork(unsigned long clone_flags,
 				!capable(CAP_SETGID))
 			return -EPERM;
 	}
-
-	/* Boost CPU to the max for 1250 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid))
-		cpu_input_boost_kick_max(1250);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
