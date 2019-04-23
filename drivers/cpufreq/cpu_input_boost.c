ad8b1a733123b945b3bbe17250a0a8e99ec65df9
Rishab Nair <rishab9706@gmail.com>
2019-04-23 06:36:36 +0000

    Revert "cpu_input_boost: Block external boosts when the screen is turned off"
    This reverts commit 3a61bf31727ac9f31c6053131df619dac070feb7.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -92,9 +92,6 @@ void cpu_input_boost_kick(void)
 	if (!b)
 		return;
 
-	if (!(get_boost_state(b) & SCREEN_AWAKE))
-		return;
-
 	queue_work(b->wq, &b->input_boost);
 }
 
@@ -122,9 +119,6 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 	struct boost_drv *b = boost_drv_g;
 
 	if (!b)
-		return;
-
-	if (!(get_boost_state(b) & SCREEN_AWAKE))
 		return;
 
 	__cpu_input_boost_kick_max(b, duration_ms);
